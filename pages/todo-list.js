d7b1192522d29986b1026c29c178efcd28433826
3bo0d7 <aboodythunder@gmail.com>
2023-07-20 11:21:56 +0400

    hot-fix

--- pages/todo-list.js
+++ pages/todo-list.js
@@ -8,26 +8,26 @@ import NewTask from '../components/NewTask';
 
 
 let homeTasks = [
-    {id: '1', name: 'laundry', description: '...'},
-    {id: '2', name: 'groceries', description: '...'}
+  { id: '1', name: 'laundry', description: '...' },
+  { id: '2', name: 'groceries', description: '...' }
 ];
 
 let workTasks = [
-    {id: '1', name: 'learn react', description: '...'},
-    {id: '2', name: 'learn nextjs', description: '...'}
+  { id: '1', name: 'learn react', description: '...' },
+  { id: '2', name: 'learn nextjs', description: '...' }
 ];
 
 const TodoListPage = () => (
   <div className={styles.todoList}>
     <div className='w-75 mx-auto'>
-        <h1 className={styles.titles}>Pending tasks for home 123:</h1>
-        <Todo tasks={homeTasks} />
-        <h2>Pending tasks for work:</h2>
-        <Todo tasks={workTasks} />
-        
-        <HelloWorld initialName='Peter' className='container todo-input' />
-        <br/>
-        <NewTask ></NewTask>
+      <h1 className={styles.titles}>Pending tasks for home 123:</h1>
+      <Todo tasks={homeTasks} />
+      <h2>Pending tasks for work:</h2>
+      <Todo tasks={workTasks} />
+
+      <HelloWorld initialName='Peter' className='container todo-input' />
+      <br />
+      <NewTask ></NewTask>
     </div>
   </div>
 )
